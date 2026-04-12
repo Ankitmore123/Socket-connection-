dac49d543bfcc85f04dea65b25cc685ae8526228
Ankit <ankitmoresam108@gmail.com>
2026-04-13 01:23:09 +0530

    implemented fault tolerance in udp connection

--- client.py
+++ client.py
@@ -1,4 +1,22 @@
 import socket
+
+
 client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.sendto("hello server whiplash".encode("utf-8"),("127.0.0.1",9999))
-print(client.recvfrom(1024)[0].decode("utf-8"))
+server_destination = ('127.0.0.1', 9999)
+
+
+client.settimeout(2.0) #client will not wait forever
+for attempt in range(1,6):
+    client.sendto("hello server whiplash".encode("utf-8"),server_destination)
+    try:
+        print(f"[Attempt {attempt}] SENDING...")
+        client.sendto(b"Hello Distributed World",server_destination )
+        data,server_source =client.recvfrom(1024)
+        print(f"[SUCCESS] Received: {data.decode()}")
+        break
+    except (socket.timeout,ConnectionResetError):
+        print(f"no response for attempt{attempt}")
+        if attempt == 5:
+            print("[FAILURE] Server is unreachable after 5 tries.")
+        
+    
--- server.py
+++ server.py
@@ -1,8 +1,20 @@
+import random
 import socket
+import time
+
+import client
 server  =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server.bind(("127.0.0.1",9999))
+while True:
+    message,client_source = server.recvfrom(1024)
+    number =  random.randint(1,10)
+    if number ==1:
+        continue
+    if number == 2:
+        time.sleep(5)
+        server.sendto("hello client ".encode("utf-8"),client_source) 
+    else:
+        server.sendto("hello client ".encode("utf-8"),client_source) 
 
-message,address = server.recvfrom(1024)
-
-print(message.decode('utf-8'))
-server.sendto("hello client ".encode("utf-8"),address) 
+    print(message.decode('utf-8'))
+    
